0d744da70d3e15d8a14cab2e5ee0d16e201dc968
Dorovich <silverdovi25@gmail.com>
2024-01-23 01:21:52 +0100

    grandes mejoras y cosas nuevas

---
+++ mg.lisp
@@ -0,0 +1,42 @@
+(defparameter *mg-data* (make-hash-table :test 'equalp))
+
+;; Definitions
+
+(defclass animal ()
+  ((name
+    :initarg :name)
+   (nature
+    :initarg :nature)
+   (aspects
+    :initarg :aspects)
+   (description
+    :initarg :description)))
+
+(defmacro add-animal (name nature aspects description)
+  (setf (gethash name *mg-data*)
+        (make-instance 'animal
+                       :name name
+                       :nature nature
+                       :aspects aspects
+                       :description description)))
+
+(defmethod print-thing ((thing animal))
+           (format t "- ~A -~%~A~%~%Naturaleza ~A ~D~%~{~A~^, ~}~%"
+                   (slot-value thing 'name)
+                   (slot-value thing 'description)
+                   (slot-value thing 'name)
+                   (slot-value thing 'nature)
+                   (slot-value thing 'aspects)))
+
+;; Animal instances
+
+(add-animal "Coyote" 8 ("Inteligente" "Adaptable" "Impredecible" "Tenaz")
+            "Más allá de las Fronteras Olfativas acecha un gran múmero de animales peligrosos. Motivados por la escasez durente el largo invierno, a veces los coyotes hacen incursiones hasta los Territorios en busca de comida.
+Los coyotes son más grandes que los zorros, pero no tan grandes como los lobos. Son omnívoros muy astutos, que lo mismo devorarán una ciudad llena de ratones que se contentarán con agarrar uno o dos ratones extraviados.")
+
+(add-animal "Abeja" 2 ("Zumbar" "Recoger Polen" "Moverse en Enjambre")
+            "Las abejas habitan dentro de sus colmenas, de cuyo interior los apicultores recogen miel y cera.")
+
+;; End
+
+(setf *last-loaded-data* *mg-data*)
--- rpg.lisp
+++ rpg.lisp
@@ -1,31 +1,51 @@
 ;; Funciones principales
 
-(defparameter *valid-commands* '(("repl" . repl)
-                                 ("roll" . roll)
-                                 ("r" . roll)
-                                 ("coin" . coin)
-                                 ("c" . coin)
-                                 ("info" . info)
-                                 ("i" . info)
-                                 ))
+(defparameter *valid-commands* '((("repl") . rpg/repl-startup)
+                                 (("roll" "r") . rpg/roll)
+                                 (("coin" "c") . rpg/coin)
+                                 (("info" "i") . rpg/info)
+                                 (("new" "n") . rpg/new)
+                                 (("do" "d") . rpg/do)))
 
 (defun main (args)
   (when (null args) (return-from main))
-  (let ((f (cdr (assoc (car args) *valid-commands* :test #'string=))))
+  (let ((f (cdr (assoc (car args) *valid-commands*
+                       :test (lambda (key item)
+                               (member key item :test 'string=))))))
     (if f
         (apply f (cdr args))
-      (format t "Ese comando no existe.~%"))))
+      (format t "[!] El comando ~A no existe.~%" (car args)))))
 
 ;; Funciones de utilidad
 
 (require :uiop)
+(require :sb-posix)
+
+(defparameter *loaded-games* (make-hash-table :test 'equal))
+(defparameter *last-loaded-data* nil)
+
+(defparameter *active-things* nil)
+(defparameter *in-repl* nil)
+
+(defgeneric print-thing (thing))
+
+(defun require-game (game)
+  "Carga el contenido del juego si no lo estaba ya."
+  (let ((%f (concatenate 'string game ".lisp")))
+    (if (and (null (gethash game *loaded-games*))
+             (probe-file %f))
+        (progn
+          (load %f)
+          (setf (gethash game *loaded-games*) *last-loaded-data*))
+      (progn
+        (format t "[!] No se han podido cargar los datos del juego ~A: Achivo ~A no encontrado.~%" game %f)
+        nil))))
 
 (defun delimiter-split (delimiter sequence
                                   &key (keep-delimiters nil)
                                   &aux (end (length sequence)))
   (loop for start = 0 then (1+ pos)
         for pos   = (position delimiter sequence :start start)
-        
         when (and (null pos) (not (= start end))) ; no more delimiter found
         collect (subseq sequence start)
         while pos ; while delimiter found
@@ -40,18 +60,37 @@
 
 ;; Funciones de interfaz
 
-(defun repl ()
+(defun rpg/repl ()
   "Invoca un REPL. Por si se quieren poner muchos comandos a la vez."
   (let ((cmd (prompt-read ">")))
     (unless (string= cmd "exit")
       (main (delimiter-split #\Space cmd))
-      (repl))))
+      (rpg/repl))))
 
-(defun roll (&optional expr)
+(defun rpg/repl-startup (&rest rest)
+  "Prepara el entorno del REPL y captura cualquier error o interrupción que haya mientras se ejecuta."
+  ;; errores?
+  (when rest
+    (format t "[!] Demasiados argumentos.~%")
+    (return-from rpg/repl-startup))
+  ;; codigo
+  (setf *in-repl* t)
+  (setf *active-things* (make-hash-table :test 'equal))
+  (handler-case
+      (rpg/repl)
+    (sb-sys:interactive-interrupt () (sb-ext:exit :code 1))
+    (error (c) (sb-ext:exit :code 1))))
+
+(defun rpg/roll (&optional expr &rest rest)
   "Tirar dados según la expresión XdY (X = cantidad, Y = caras) y calcular la suma de los resultados."
+  ;; errores?
   (when (null expr)
-    (format t "Esta función necesita un argumento: expresión de dados.~%")
-    (return-from roll))
+    (format t "[?] Esta función necesita un argumento: expresión de dados.~%")
+    (return-from rpg/roll))
+  (when rest
+    (format t "[!] Demasiados argumentos.~%")
+    (return-from rpg/roll))
+  ;; codigo
   (let* ((nums (delimiter-split #\d expr))
          (times (parse-integer (car nums)))
          (faces (parse-integer (cadr nums)))
@@ -61,11 +100,16 @@
           do (incf result (+ 1 (random faces))))
     (format t "~Dd~D => ~D~%" times faces result)))
 
-(defun coin (&optional times)
+(defun rpg/coin (&optional times &rest rest)
   "Tirar X cantidad de monedas y hacer recuendo de la cantidad de caras y cruces."
+  ;; errores?
   (when (null times)
-    (format t "Esta función necesita un argumento: número de monedas.~%")
-    (return-from coin))
+    (format t "[?] Esta función necesita un argumento: número de monedas.~%")
+    (return-from rpg/coin))
+  (when rest
+    (format t "[!] Demasiados argumentos.~%")
+    (return-from rpg/coin))
+  ;; codigo
   (let ((heads 0)
         (tails 0))
     (setf times (parse-integer times))
@@ -76,12 +120,53 @@
                (incf heads)))
     (format t "~D monedas => ~D caras, ~D cruces~%" times heads tails)))
 
-(defun info (&optional game topic)
+(defun rpg/info (&optional game topic &rest rest)
   "Buscar información sobre un tema concreto de un juego en concreto."
+  ;; errores?
   (when (or (null game) (null topic))
-    (format t "Esta función necesita dos argumentos: nombre del juego, nombre del tema en concreto.~%")
-    (return-from info))
-  (format t "Buscando informacion sobre ~A del juego ~A...~%" topic game))
+    (format t "[?] Esta función necesita dos argumentos: nombre del juego, nombre del tema en concreto.~%")
+    (return-from rpg/info))
+  (unless (require-game game)
+    (return-from rpg/info))
+  ;; codigo
+  (let* ((game-data (gethash game *loaded-games*))
+         (thing (gethash topic game-data)))
+    (print-thing thing)))
+
+(defun rpg/new (&optional game topic identifier &rest rest)
+  "Crear una instancia de un tema para usar en otros comandos. Para uso en el REPL."
+  ;; errores?
+  (unless *in-repl*
+    (format t "[!] Las instancias se eliminan cuando el programa finaliza. Para trabajar con ellas usa el REPL: ./rpg repl~%")
+    (return-from rpg/new))
+  (when (or (null game) (null topic) (null identifier))
+    (format t "[?] Esta función necesita tres argumentos: nombre del juego, nombre de la cosa en concreto, nombre identificativo exclusivo.~%")
+    (return-from rpg/new))
+  (when rest
+    (format t "[!] Demasiados argumentos.~%")
+    (return-from rpg/new))
+  (unless (require-game game)
+    (return-from rpg/new))
+  ;; codigo
+  (let* ((game-data (gethash game *loaded-games*))
+         (thing (gethash topic game-data)))
+    (setf (gethash identifier *active-things*) thing)))
+
+(defun rpg/do (&optional action identifier &rest rest)
+  "Hacer una acción sobre una instancia. Para uso en el REPL."
+  ;; errores?
+  (unless *in-repl*
+    (format t "[!] Las instancias se eliminan cuando el programa finaliza. Para trabajar con ellas usa el REPL: ./rpg repl~%")
+    (return-from rpg/do))
+  (when (or (null action) (null identifier))
+    (format t "[?] Esta función necesita dos argumentos: acción a realizar, nombre identificativo exclusivo.~%")
+    (return-from rpg/do))
+  (when rest
+    (format t "[!] Demasiados argumentos.~%")
+    (return-from rpg/do))
+  ;; codigo
+  (if (string= action "print")
+      (print-thing (gethash identifier *active-things*))))
 
 ;; Inicio del programa
 
